28e60ee9ad7bac6ba22e3f5b91c29a22e4c08aaf
devvspaces <netrobepy@gmail.com>
2021-12-06 06:36:17 +0100

    made sure gen function creates a readme file

--- pyreadme/generate.py
+++ pyreadme/generate.py
@@ -3,9 +3,11 @@ import requests
 link = 'https://raw.githubusercontent.com/devvspaces/readme_template/master/README.md'
 
 def gen():
-    return requests.get(url=link).text
+    with open('README.md', 'w', encoding="utf-8") as readme:
+        file = requests.get(url=link).text
+        readme.write(file)
 
 
 if __name__ == "__main__":
     print('Calling the gen func')
-    print(gen())
+    gen()
